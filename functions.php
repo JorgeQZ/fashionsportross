2734b42b60c999fc6a124c319eb768e4c440f8db
JorgeQz <jorge_qzg@hotmail.com>
2019-09-27 17:15:39 -0500

    Init

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+add_action('after_setup_theme', 'register_custom_nav_menus');
+function register_custom_nav_menus()
+{
+    register_nav_menus(array(
+        'Header' => 'Header Navigation Menu',
+        // 'footer_menu' => 'My Custom Footer Menu',
+    ));
+}
+
+
+// Register style sheet.
+add_action('wp_enqueue_scripts', 'register_theme_styles');
+
+/**
+ * Register style sheet.
+ */
+function register_theme_styles()
+{
+    wp_enqueue_style('header', get_template_directory_uri() . '/css/header.css', array(), filemtime(get_template_directory() . '/css/header.css'), false);
+    wp_enqueue_style('montserrat', get_template_directory_uri() . '/fonts/Montserrat.css', array(), filemtime(get_template_directory() . '/fonts/Montserrat.css'), false);
+    wp_enqueue_script('jquery');
+}
---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+
+    <header class="main-header">
+        <a href="" class="brand-logo">
+            <img src="<?php echo get_template_directory_uri() . '/img/Logo.png'; ?>" alt="" class="main-logo">
+        </a>
+
+        <div class="menu-cont">
+            <ul>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+                <li><a href="">test</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="wrapper">
