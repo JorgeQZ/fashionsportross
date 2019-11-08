719c504efaae4e2593e16fa142111c88738d5b4d
Akevia <akevia@Akevias-iMac.local>
2019-11-08 17:27:13 -0600

    pagina semi

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+add_theme_support( 'title-tag' );
 add_action('after_setup_theme', 'register_custom_nav_menus');
 function register_custom_nav_menus()
 {
--- header.php
+++ header.php
@@ -5,7 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <?php
+    if ( ! function_exists( '_wp_render_title_tag' ) ) {
+        function theme_slug_render_title() {
+            ?>
+            <title><?php wp_title( '|', true, 'right' ); ?></title>
+            <?php
+            }
+            add_action( 'wp_head', 'theme_slug_render_title' );
+        }
+        ?>
     <?php wp_head(); ?>
 </head>
 
