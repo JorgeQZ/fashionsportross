08bd7a908bdea9ab46e9c14805cf52a2f3bf1711
Akevia <akevia@Akevias-iMac.local>
2020-01-03 15:54:03 -0600

    Correcto

--- header.php
+++ header.php
@@ -9,43 +9,44 @@
     if ( ! function_exists( '_wp_render_title_tag' ) ) {
         function theme_slug_render_title() {
             ?>
-        <title>
-            <?php wp_title( '|', true, 'right' ); ?>
-        </title>
-        <?php
+    <title>
+        <?php wp_title( '|', true, 'right' ); ?>
+    </title>
+    <?php
             }
             add_action( 'wp_head', 'theme_slug_render_title' );
         }
         ?>
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <?php wp_head(); ?>
 </head>
 
 <body class="wp_body">
     <header class="main-header">
-    <div class="contain-search-bar">
+        <div class="contain-search-bar">
             <?php dynamic_sidebar( 'Buscador' );?>
         </div>
         <div class="header-content">
-      
-        <a href="<?php home_url() ?>" class="brand-logo">
-            <img src="<?php echo get_template_directory_uri() . '/img/Logo.png'; ?>" alt="" class="main-logo">
-        </a>
-        <div class="container-cuenta" id="container-id">
-            <div class="contain-account">
-                <div class="column">
-                    <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'Ve tu carrito' ); ?>">
-                        Carrito
-                        <img src="<?php echo get_template_directory_uri().'/img/icono-carrito.png'; ?>" alt="">
-                        (<?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() );?>)
-                    </a>
-                </div>
-            </div>
 
-            <div class="contain-search" >
-                <img src="<?php echo get_template_directory_uri().'/img/search.png' ?>" alt="" id="search-button">
-            </div>
-            <?php
+            <a href="<?php home_url() ?>" class="brand-logo">
+                <img src="<?php echo get_template_directory_uri() . '/img/Logo.png'; ?>" alt="" class="main-logo">
+            </a>
+            <div class="container-cuenta" id="container-id">
+                <div class="contain-account">
+                    <div class="column">
+                        <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>"
+                            title="<?php _e( 'Ve tu carrito' ); ?>">
+                            Carrito
+                            <img src="<?php echo get_template_directory_uri().'/img/icono-carrito.png'; ?>" alt="">
+                            (<?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() );?>)
+                        </a>
+                    </div>
+                </div>
+
+                <div class="contain-search">
+                    <img src="<?php echo get_template_directory_uri().'/img/search.png' ?>" alt="" id="search-button">
+                </div>
+                <?php
                 wp_nav_menu(array(
                 'menu'              => "Header",
                 'menu_id'              => "main_menu",
@@ -55,14 +56,13 @@
             ));
             ?>
 
-        </div>
+            </div>
 
-        <div class="mobile-button" id="mobile-button">
-            <div class="bars"></div>
-            <div class="bars"></div>
-            <div class="bars"></div>
-        </div>
+            <div class="mobile-button" id="mobile-button">
+                <div class="bars"></div>
+                <div class="bars"></div>
+                <div class="bars"></div>
+            </div>
         </div>
     </header>
-a
     <div class="wrapper">
